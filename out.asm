a4e6e24c8123576c30a2b76fd7ce5f7af8407534
DominikSzasz <dominik@szaszdominik.hu>
2024-01-10 21:15:01 +0100

    restructuring project

--- out.asm
+++ out.asm
@@ -1,19 +1,5 @@
 section .data
 	hello: db "hello", 10
-	hello2: db "hello2", 10
-	hello3: db "hello3", 10
-	hello4: db "hello4", 10
-	hello5: db "hello5", 10
-	hello6: db "hello6", 10
-	hello7: db "hello7", 10
-	hello8: db "hello8", 10
-	hello9: db "hello9", 10
-	helloa: db "helloa", 10
-	hellob: db "hellob", 10
-	helloc: db "helloc", 10
-	hellod: db "hellod", 10
-	helloe: db "helloe", 10
-	hellof: db "hellof", 10
 section .text
 	global _main
 _main:
@@ -22,76 +8,6 @@ _main:
 	mov rsi, hello
 	mov rdx, 5
 	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, hello2
-	mov rdx, 6
-	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, hello3
-	mov rdx, 6
-	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, hello4
-	mov rdx, 6
-	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, hello5
-	mov rdx, 6
-	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, hello6
-	mov rdx, 6
-	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, hello7
-	mov rdx, 6
-	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, hello8
-	mov rdx, 6
-	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, hello9
-	mov rdx, 6
-	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, helloa
-	mov rdx, 6
-	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, hellob
-	mov rdx, 6
-	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, helloc
-	mov rdx, 6
-	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, hellod
-	mov rdx, 6
-	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, helloe
-	mov rdx, 6
-	syscall
-	mov rax, 0x02000004
-	mov rdi, 1
-	mov rsi, hellof
-	mov rdx, 6
-	syscall
 	mov rax, 0x02000001
-	mov rdi, 69
+	mov rdi, 0
 	syscall
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
-g++ -std=c++17 -o main main.cpp parsing.cpp statementize.cpp generation.cpp
+g++ -std=c++17 -o ./build/main ./src/main.cpp ./src/parsing.cpp ./src/statementize.cpp ./src/generation.cpp
+cd ./build
 ./main
-nasm -f macho64 out.asm
-ld out.o -o out -macosx_version_min 10.15.7 -lSystem
+nasm -f macho64 ./out.asm
+ld ./out.o -o ../out -macosx_version_min 10.15.7 -lSystem
+cd ../
 ./out
